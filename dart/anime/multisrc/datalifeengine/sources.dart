048ea74cc331f909711c53b9a2fcc3e577ea9973
yxxyun <sean4yang@gmail.com>
2024-09-23 10:52:46 +0800

    Update sources.dart

--- dart/anime/multisrc/datalifeengine/sources.dart
+++ dart/anime/multisrc/datalifeengine/sources.dart
@@ -4,7 +4,7 @@ import 'src/wiflix/wiflix.dart';
 
 const _datalifeengineVersion = "0.0.45";
 const _datalifeengineSourceCodeUrl =
-    "https://raw.githubusercontent.com/kodjodevf/mangayomi-extensions/$branchName/dart/anime/multisrc/datalifeengine/datalifeengine.dart";
+    "https://raw.githubusercontent.com/yxxyun/mangayomi-extensions/$branchName/dart/anime/multisrc/datalifeengine/datalifeengine.dart";
 
 List<Source> get datalifeengineSourcesList => _datalifeengineSourcesList;
 List<Source> _datalifeengineSourcesList = [
